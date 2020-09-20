b9537843e4d7e2f6c0bbbf95cb37d30d3fa5ad21
AndyMhlanga <50343712+andymhlanga@users.noreply.github.com>
2020-09-20 11:30:01 -0600

    tweaked java file to output to console

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-console.log("This is Java console log message")
 
-const ShippingButton = document.getElementById("label");
-const ShippingWeight = document.getElementById("weight");
-const ShippingRateDisplay = document.getElementById(".shipping-rate");
-const ShippingRate = 5;
+// Get Reference Variables to the input and label elements
+const calculateButton = document.querySelector('label')
+const shippingWeight = document.querySelector('input')
+const shippingRateDisplay = document.querySelector('.shipping-rate')
 
+// create ref var  to the logo
+// add a click event to the logo and log text to the console
+
+// create ref var to the login button
+// add click event to the login button log you want to login
+/* 
+Template Literal 
+   `sdfsdafsfsdf  ${your js code now}`
+    console.log(`ldflfjlsjflsjflsajf   ${shippingRate}` )
+    console.log(`<p>${shippingRate}</p>`)
+*/
+
+//  Event
+calculateButton.addEventListener('click', function(){
+    // read in the value of the input element
+    // calculate the shipping rate based on the input
+    // echo the rate to the console.
+    //let rate =   shippingWeight.value * shippingRate;
+    //shippingRateDisplay.textContent = '$' + rate + '.00';
+    //shippingRateDisplay.style.fontSize = "3rem"
+    console.log(shippingWeight.value);
+})
 
