9ae06f2a0628aa743fd6ed4c1a6911618393afe6
AndyMhlanga <50343712+andymhlanga@users.noreply.github.com>
2020-09-20 11:19:50 -0600

    Added the console messge and variables

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("This is Java console log message")
+
+const ShippingButton = document.getElementById("label");
+const ShippingWeight = document.getElementById("weight");
+const ShippingRateDisplay = document.getElementById(".shipping-rate");
+const ShippingRate = 5;
+
+
